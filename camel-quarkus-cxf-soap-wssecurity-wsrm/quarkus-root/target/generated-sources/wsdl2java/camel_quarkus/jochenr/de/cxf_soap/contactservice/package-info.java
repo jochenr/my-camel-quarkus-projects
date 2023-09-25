cc9dd3f9f08757098ea0b5710afb7c9d7135b406
a000478 <Jochen.Riedlinger@l-bank.de>
2023-09-25 10:52:29 +0200

    cleanup accidentally committd files from target dir

--- camel-quarkus-cxf-soap-wssecurity-wsrm/quarkus-root/target/generated-sources/wsdl2java/camel_quarkus/jochenr/de/cxf_soap/contactservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://de.jochenr.camel-quarkus/cxf-soap/ContactService", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package camel_quarkus.jochenr.de.cxf_soap.contactservice;
